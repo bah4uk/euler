acf3a2782649b67b0dcc7cb270fd675cc35ad296
Ivan Danci <idanci@nebulent.com>
2013-03-16 17:48:25 +0200

    Add exercise 13

--- app/domain/euler.rb
+++ app/domain/euler.rb
@@ -1,5 +1,8 @@
 class Euler
 
+  CURRENT_DIR   = File.dirname(__FILE__)
+  NUMBERS_PATH = File.join(CURRENT_DIR, '../exercises/problem13.txt')
+
   def self.ex1
     (1...1000).to_a.select{|c| (c%3).zero? or (c%5).zero?}.inject(:+)
   end
@@ -12,6 +15,10 @@ class Euler
   #   end
   #   arr.select{|c| c.even?}.inject(:+)
   # end
+  def self.ex13
+    num = File.open(NUMBERS_PATH, "rb").read.split.map{|c| c.to_i}
+                                      .inject(:+).to_s[0..9].to_i
+  end
 
   def self.ex16
     (2**1000).to_s.scan(/./).map{|c| c.to_i}.inject(:+)
