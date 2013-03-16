632710b05be59299f46f4dacd7a1cb15f2088e8c
Ivan Danci <idanci@nebulent.com>
2013-03-16 10:16:31 +0200

    Initial commit :airplane:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euler::Application.config.secret_token = 'ece253f04a9d7abec9965bd481963feb2f5c684c4b654f7776e9ed4c87359c07c5d96bec59c1aefbd0f991fcf405e4da5875e808078b3cc02eedeabe19d2449c'
